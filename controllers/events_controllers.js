b802e2e8bdc288d5996fc5cebd36708f91d823fc
PapineauA <papineautaidan@gmail.com>
2023-01-24 20:38:12 -0500

    Uploading finished music tour

--- controllers/events_controllers.js
+++ controllers/events_controllers.js
@@ -1,10 +1,10 @@
-//DEPENDENCIES
+// DEPENDENCIES
 const events = require("express").Router();
 const db = require("../models");
-const { Event } = db;
+const { Event, MeetGreet, SetTime, Stage, Band } = db;
 const { Op } = require("sequelize");
 
-//FINDING ALL EVENTS
+// FIND ALL EVENTS
 events.get("/", async (req, res) => {
   try {
     const foundEvents = await Event.findAll({
@@ -19,11 +19,36 @@ events.get("/", async (req, res) => {
   }
 });
 
-//FIND A SPECIFIC EVENT
-events.get("/:id", async (req, res) => {
+// FIND A SPECIFIC EVENT
+events.get("/:name", async (req, res) => {
   try {
     const foundEvent = await Event.findOne({
-      where: { event_id: req.params.id },
+      where: { name: req.params.name },
+      include: [
+        {
+          model: MeetGreet,
+          as: "meet_greets",
+          attributes: { exclude: ["event_id", "band_id"] },
+          include: {
+            model: Band,
+            as: "band",
+          },
+        },
+        {
+          model: SetTime,
+          as: "set_times",
+          attributes: { exclude: ["event_id", "stage_id", "band_id"] },
+          include: [
+            { model: Band, as: "band" },
+            { model: Stage, as: "stage" },
+          ],
+        },
+        {
+          model: Stage,
+          as: "stages",
+          through: { attributes: [] },
+        },
+      ],
     });
     res.status(200).json(foundEvent);
   } catch (error) {
--- models/band.js
+++ models/band.js
@@ -1,5 +1,6 @@
 "use strict";
 const { Model } = require("sequelize");
+const meetgreet = require("./meetgreet");
 module.exports = (sequelize, DataTypes) => {
   class Band extends Model {
     /**
@@ -7,8 +8,16 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
-      // define association here
+    static associate(MeetGreet, SetTime) {
+      // Meet & Greets
+      Band.hasMany(MeetGreet, {
+        foreignKey: "band_id",
+        as: "meet_greets",
+      });
+      Band.hasMany(SetTime, {
+        foreignKey: "band_id",
+        as: "meet_greets",
+      });
     }
   }
   Band.init(
